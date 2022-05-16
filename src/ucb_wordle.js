a067d70098c45074af490e2795970c7940015722
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-05-16 01:01:38 -0400

    adivinar: refactor de adivinador

--- src/ucb_wordle.js
+++ src/ucb_wordle.js
@@ -2,11 +2,10 @@ function description() {
   return "UCB Wordle es un divertido juego de palabras en el que se tiene que adivinar una palabra con 6 intentos en cada intento se mostrará las letras en las que hay coincidencias tanto en forma como posicion";
 }
 function adivinar(palabra, adivinanza){
-  if (palabra === adivinanza) {
-    return "EXISTE";
+  let comparativa = "EXISTE";
+  if (palabra != adivinanza) {
+    comparativa = "NO " + comparativa;
   }
-  else{
-    return "NO EXISTE";
-  }
+  return comparativa;
 }
 export {description, adivinar};
