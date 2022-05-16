e0da0da8a742ff289bca7e94170257dc7770d472
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-05-15 23:48:05 -0400

    adivinar test no pasan la prueba

--- src/ucb_wordle.test.js
+++ src/ucb_wordle.test.js
@@ -5,3 +5,8 @@ describe("Descripcion", () => {
     expect(description()).toEqual("UCB Wordle es un divertido juego de palabras en el que se tiene que adivinar una palabra con 6 intentos en cada intento se mostrará las letras en las que hay coincidencias tanto en forma como posicion");
   });
 });
+describe("adivinar", () => {
+  it("deberia verificar si una palabra fue adivinada", () => {
+    expect(adivinar("hello")).toEqual("EXISTE");
+  });
+});
