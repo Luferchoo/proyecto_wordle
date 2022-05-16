45e451f27ad163779415b3f8f15245a0b5ffbb9f
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-05-15 23:24:21 -0400

    descripcion se muestra con exito en interfaz

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import sumar from "./sumador";
+import descripcion from "./ucb_wordle";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
@@ -11,5 +11,5 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + descripcion() + "</p>";
 });
