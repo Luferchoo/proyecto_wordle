0356382983baed5e1b0b313e9d394518f4160804
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-05-16 22:31:09 -0400

    longitud de las palabras funciones refactorizadas y modularizadas

--- src/ucb_wordle.js
+++ src/ucb_wordle.js
@@ -1,17 +1,29 @@
 function description() {
   return "UCB Wordle es un divertido juego de palabras en el que se tiene que adivinar una palabra con 6 intentos en cada intento se mostrará las letras en las que hay coincidencias tanto en forma como posicion";
 }
+function adivinanza_vacia(adivinanza,comparacion){
+  if (adivinanza === "") {
+    comparacion = "Ingrese una palabra para jugar";
+  }
+  return comparacion;
+}
+function adivinanza_longitud(palabra, adivinanza,comparacion){
+  if ( palabra.length != adivinanza.length) {
+    comparacion = "Ingrese una palabra de la misma longitud para jugar";
+  }
+  return comparacion;
+}
+function comparativa_palabras(palabra, adivinanza,comparacion){
+  if (palabra != adivinanza) {
+    comparacion = "NO " + comparacion;
+  }
+  return comparacion;
+}
 function adivinar(palabra, adivinanza){
   let comparativa = "EXISTE";
-  if (adivinanza === "") {
-    comparativa = "Ingrese una palabra para jugar";
-  }
-  else if ( palabra.length != adivinanza.length) {
-    comparativa = "Ingrese una palabra de la misma longitud para jugar";
-  }
-  else if (palabra != adivinanza) {
-    comparativa = "NO " + comparativa;
-  }
+  comparativa = comparativa_palabras(palabra,adivinanza,comparativa);
+  comparativa = adivinanza_longitud(palabra,adivinanza,comparativa);
+  comparativa = adivinanza_vacia(adivinanza,comparativa);
   return comparativa;
 }
 export {description, adivinar};
